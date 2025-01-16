4b67205b14344cf297e1affe7ea93d4fe4bfadeb
janginh <znfnvkd456@naver.com>
2025-01-16 13:36:34 +0900

    폴더 추가

---
+++ C++/backjoon/실버/1747_소수&팰린드롬.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+bool check_sosu(int a){
+    for(int i=2; i<a/2; i++){
+        if(a % i == 0){
+            return 0;
+        }
+        else continue;
+    }
+    return 1;
+}
+bool check_pal(int a){
+    for()
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    bool sosu = check_sosu(n);
+    if(sosu == true && ){
+
+    }
+
+    return 0;
+}
